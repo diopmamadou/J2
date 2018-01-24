2202cc441159aa2d41529a4697a3b75c0ff7c728
diopmamadou <mamadou.diop@groupe-esigelec.org>
2018-01-24 11:08:45 +0100

    modified main class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("START MAIN");
+		System.out.println("START MAIN TEST");
 		System.out.println(new Joueur("Estelle",25,"Equipe 21"));
 
 		System.out.println("----------------------------------------------------------------------------------\nEND TEST\n"+
